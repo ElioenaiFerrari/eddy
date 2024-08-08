d57533ca4745c8cc5e3a4119c548a3d1c6a91944
Elioenai Pinheiro Ferrari <elioenai.ferrari@ifood.com.br>
2024-08-08 14:53:07 -0300

    fix assets

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,6 @@
 use bevy::prelude::*;
 use eddy::prelude::*;
 
-fn game_acceleration(
-    mut acceleration_timer: ResMut<AccelerationTimer>,
-    time: Res<Time>,
-    mut query_game: Query<&mut Acceleration>,
-) {
-    if acceleration_timer.0.tick(time.delta()).just_finished() {
-        let mut acceleration = query_game.single_mut();
-
-        acceleration.0 = Vec2::new(acceleration.0.x + 1., acceleration.0.y + 1.);
-    }
-}
-
-fn is_not_ended(query_player: Query<&Health, With<Player>>) -> bool {
-    let player_health = query_player.single();
-
-    player_health.0 > 0
-}
-
 fn main() {
     App::new()
         .add_plugins((
@@ -26,10 +8,5 @@ fn main() {
             GamePlugin,
             PlayerPlugin,
         )) // prevents blurry sprites
-        .add_systems(
-            Update,
-            game_acceleration.run_if(is_not_ended),
-            // player_jump.run_if(is_player_in_air),
-        )
         .run();
 }
--- src/plugins/game.rs
+++ src/plugins/game.rs
@@ -16,19 +16,41 @@ fn setup(mut commands: Commands, assets_server: Res<AssetServer>) {
         0.008,
         TimerMode::Repeating,
     )));
-    commands.insert_resource(JumpTimer(Timer::from_seconds(0.001, TimerMode::Repeating)));
+    commands.insert_resource(JumpTimer(Timer::from_seconds(0.1, TimerMode::Repeating)));
     commands.insert_resource(AccelerationTimer(Timer::from_seconds(
         30.0,
         TimerMode::Repeating,
     )));
     commands.insert_resource(AnimationTimer(Timer::from_seconds(
-        0.08,
+        0.1,
         TimerMode::Repeating,
     )));
 }
 
+fn game_acceleration(
+    mut acceleration_timer: ResMut<AccelerationTimer>,
+    time: Res<Time>,
+    mut query_game: Query<&mut Acceleration>,
+) {
+    if acceleration_timer.0.tick(time.delta()).just_finished() {
+        let mut acceleration = query_game.single_mut();
+
+        acceleration.0 = Vec2::new(acceleration.0.x + 1., acceleration.0.y + 1.);
+    }
+}
+
+fn is_not_ended(query_player: Query<&Health, With<Player>>) -> bool {
+    let player_health = query_player.single();
+
+    player_health.0 > 0
+}
+
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup);
+        app.add_systems(Startup, setup).add_systems(
+            Update,
+            game_acceleration.run_if(is_not_ended),
+            // player_jump.run_if(is_player_in_air),
+        );
     }
 }
